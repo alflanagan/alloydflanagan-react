c3f1f9328fcd83e6caf32843140961525969997a
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2020-11-22 15:28:35 -0500

    Styling for header component

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,20 +1,21 @@
 /* eslint jsx-quotes: ["error", "prefer-double"] */
 import React from 'react'
+import './Header.css'
 
 export const Header = () => {
   return (
-    <header class="masthead mb-auto">
-      <div class="inner">
-        <h3 class="masthead-brand">Adrian L Flanagan</h3>
-        <nav class="nav nav-masthead justify-content-center app-nav">
-          <span class="nav-link"><a href="/">Home</a></span>
-          <span class="nav-link"><a href="/about_me">About Me</a></span>
-          <span class="nav-link"><a href="/contact">Contact Me</a></span>
-          <span class="nav-link"><a href="/blog">Blog</a></span>
-          <span class="nav-link"><a href="/portfolio">Portfolio</a></span>
-          <span class="nav-link"><a href="/accounts/logout">Logout</a></span>
-          <span class="nav-link"><a href="/accounts/login">Login</a></span>
-          <span class="nav-link"><a href="/accounts/signup">Register</a></span>
+    <header className="masthead mb-auto">
+      <div className="inner">
+        <h3 className="masthead-brand">Adrian L Flanagan</h3>
+        <nav className="nav nav-masthead justify-content-center app-nav">
+          <span className="nav-link"><a href="/">Home</a></span>
+          <span className="nav-link"><a href="/about_me">About Me</a></span>
+          <span className="nav-link"><a href="/contact">Contact Me</a></span>
+          <span className="nav-link"><a href="/blog">Blog</a></span>
+          <span className="nav-link"><a href="/portfolio">Portfolio</a></span>
+          <span className="nav-link"><a href="/accounts/logout">Logout</a></span>
+          <span className="nav-link"><a href="/accounts/login">Login</a></span>
+          <span className="nav-link"><a href="/accounts/signup">Register</a></span>
         </nav>
       </div>
     </header>
