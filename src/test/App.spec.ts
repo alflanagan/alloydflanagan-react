bad10ee49655ff9de20a314d9eea3405e02bc82a
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2022-09-05 18:05:16 -0400

    set up mocha, chai for testing

--- src/test/App.spec.ts
+++ src/test/App.spec.ts
@@ -1,13 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
+import { expect } from 'chai'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  )
-  expect(screen.getByText(/Adrian L Flanagan/)).toBeInTheDocument()
+describe('class App', function () {
+  it('renders my name', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    expect(screen.getByText(/Adrian L Flanagan/)).to.equal('fred')
+  })
 })
