e831670bf6020bdbb3ea9721c3a174f18e5e543f
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2022-09-10 17:47:01 -0400

    install stylelint; reformat css files

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App (): JSX.Element {
   const x = (
-    <div className="App">
+    <div className="app">
       <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
         <Header />
         <div id="content" />
